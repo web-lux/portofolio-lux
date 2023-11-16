34b409076925aa106da056e80b0388bff65233b4
lux <web-lux@outlook.fr>
2023-11-16 15:07:01 +0100

    add wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,11 @@ import Footer from "./Footer"
 
 function App() {
   return (
-    <>
+    <div className="wrapper">
       <Header />
       <Outlet />
       <Footer />
-    </>
+    </div>
   )
 }
 
