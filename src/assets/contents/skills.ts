52c588fed9857f941e15fffa6225071fba01d787
lux <web-lux@outlook.fr>
2023-11-18 17:42:53 +0100

    add skills section

---
+++ src/assets/contents/skills.ts
@@ -0,0 +1,23 @@
+export const skills = [
+	{
+		title: "Développement front-end",
+		description:
+			"Être capable d’intégrer les sites que je maquette, avec une priorité donnée à la propreté du code pour ménager le travail de mes collaborateurs  et le responsive, c’est à dire le fait que le site internet soit lisible peu importe le support utilisé pour le consulter.",
+		tools: ["HTML", "CSS", "Javascript", "Bootstrap", "SASS"],
+		icon: "code",
+	},
+	{
+		title: "Design graphique",
+		description:
+			"Désigner des sites modernes qui répondent aux besoins d’un client grâce à l’élaboration d’un cahier des charges en adéquation avec sa demande, sa vision, suppléées par mes propres notions de design.",
+		tools: ["Photoshop", "Illustrator"],
+		icon: "paintbrush",
+	},
+	{
+		title: "Design UX • UI",
+		description:
+			"Créer des supports de communication pour un client, la charte graphique de son entreprise ou les ressources nécessaires à certaines sections de son site internet comme des pictogrammes, ou des photographies dont les couleurs ont été harmonisées par logiciel.",
+		tools: ["Figma", "Adobe XD"],
+		icon: "eye",
+	},
+];
