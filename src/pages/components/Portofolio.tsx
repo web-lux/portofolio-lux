3cccac259c004d6788e41964ceb95dad0b3738c4
lux <web-lux@outlook.fr>
2023-11-19 17:34:07 +0100

    add portofolio section style

--- src/pages/components/Portofolio.tsx
+++ src/pages/components/Portofolio.tsx
@@ -1,3 +1,98 @@
 export default function Portofolio() {
-	return <section>Portofolio</section>;
+	return (
+		<section id="portofolio">
+			<h2 className="h2">Mon portofolio</h2>
+			<div className="projects">
+				<div className="project">
+					<div className="img shadow">
+						<img src="/runzik.jpg" alt="" />
+					</div>
+					<div className="header">
+						<h3>Run'Zik</h3>
+						<p>
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
+							molestias laudantium natus earum repudiandae libero ea debitis
+							possimus. Ex cupiditate officiis minima neque vel modi corrupti
+							illo distinctio, perspiciatis delectus?
+						</p>
+						<a href="" className="btn">
+							En savoir plus
+						</a>
+					</div>
+				</div>
+
+				<div className="project">
+					<div className="img shadow">
+						<img src="/alliance.jpg" alt="" />
+					</div>
+					<div className="header">
+						<h3>Run'Zik</h3>
+						<p>
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
+							molestias laudantium natus earum repudiandae libero ea debitis
+							possimus. Ex cupiditate officiis minima neque vel modi corrupti
+							illo distinctio, perspiciatis delectus?
+						</p>
+						<a href="" className="btn">
+							En savoir plus
+						</a>
+					</div>
+				</div>
+
+				<div className="project">
+					<div className="img shadow">
+						<img src="/api-connect.jpg" alt="" />
+					</div>
+					<div className="header">
+						<h3>Run'Zik</h3>
+						<p>
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
+							molestias laudantium natus earum repudiandae libero ea debitis
+							possimus. Ex cupiditate officiis minima neque vel modi corrupti
+							illo distinctio, perspiciatis delectus?
+						</p>
+						<a href="" className="btn">
+							En savoir plus
+						</a>
+					</div>
+				</div>
+
+				<div className="project">
+					<div className="img shadow">
+						<img src="/indiescape.jpg" alt="" />
+					</div>
+					<div className="header">
+						<h3>Run'Zik</h3>
+						<p>
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
+							molestias laudantium natus earum repudiandae libero ea debitis
+							possimus. Ex cupiditate officiis minima neque vel modi corrupti
+							illo distinctio, perspiciatis delectus?
+						</p>
+						<a href="" className="btn">
+							En savoir plus
+						</a>
+					</div>
+				</div>
+
+				<div className="project">
+					<div className="img shadow" >
+						<img src="/mecaconnect.jpg" alt="" />
+					</div>
+					<div className="header">
+						<h3>Run'Zik</h3>
+						<p>
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
+							molestias laudantium natus earum repudiandae libero ea debitis
+							possimus. Ex cupiditate officiis minima neque vel modi corrupti
+							illo distinctio, perspiciatis delectus?
+						</p>
+						<a href="" className="btn">
+							En savoir plus
+						</a>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
 }
